3be585776fc3a323c04d8355f01f1097a911f797
Abhay Rajesh Shah <abhayrajeshshah@gmail.com>
2021-10-01 21:12:56 +0530

    small style fix and navlink fix

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom"
 function Nav(){
    return <div>
     <nav className="navbar  navbar-expand-lg navbar-dark nav-bg">
-            <a className="navbar-brand" href="/" style={{color:"#ffffff"}}>Abhay</a>
+      <Link className="navbar-brand" style={{color:"white"}} to="/">Abhay</Link>
              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
